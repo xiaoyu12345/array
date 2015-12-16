88cbc0d905f3f7b6b49238292b20e402dcca9889
xiaoyu12345 <20141105060w@Mac12.local>
2015-12-16 19:21:24 +0800

    数组越界
    定义数组a和数组b，数组a先运行，数组b再运行，数组b输入超出定义范围，覆盖了数组a之前赋值的数。之后再次使用数组a 时就会出现错误

--- array/main.cpp
+++ array/main.cpp
@@ -10,6 +10,20 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[3]={1,2,3};
+    printf("a[0]=%d   a[1]=%d   a[2]=%d\n",a[0],a[1],a[2]);
+    int b[4];
+    printf("%x      %x\n",a,b);
+    int i;
+    for(i=1;i<10;i++)
+    {
+        b[i]=i;
+    }
+    printf("a[0]=%d   a[1]=%d   a[2]=%d\n",a[0],a[1],a[2]);
+    for(i=1;i<10;i++)
+    {
+        printf("%d\n",b[i]);
+    }
+    
     return 0;
 }
