2931395220aa2f9ec996ffe5f67c9a080f9593dd
xiaoyu12345 <20141105060w@Mac12.local>
2015-12-16 19:14:30 +0800

    Initial Commit

---
+++ array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  array
+//
+//  Created by 小鱼🐟 on 15/12/16.
+//  Copyright (c) 2015年 小鱼🐟. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
